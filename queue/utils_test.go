91e9fd24cacb01196919d4fbfc617082c625bc19
LiQiming <leeqiming@163.com>
2022-06-01 18:18:01 +0800

    test for util reject

--- queue/utils_test.go
+++ queue/utils_test.go
@@ -2,14 +2,13 @@ package queue
 
 import "testing"
 
+type tempS struct {
+	Id int
+}
+
 func TestFindByItem(t *testing.T) {
-	type tempS struct {
-		Id int
-	}
-	array := []*tempS{}
-	for i := 0; i < 10; i++ {
-		array = append(array, &tempS{Id: i})
-	}
+
+	array := initData()
 	item := FindByItem[tempS](
 		array, func(e1 tempS) bool {
 			return e1.Id == 5
@@ -19,4 +18,36 @@ func TestFindByItem(t *testing.T) {
 		t.Errorf("Expected 5, got %d", *item)
 	}
 
+}
+
+func TestFilterByItem(t *testing.T) {
+	data := initData()
+	item := FilterByItem(
+		data, func(e1 tempS) bool {
+			return e1.Id >= 5
+		},
+	)
+	if item[0].Id < 5 {
+		t.Errorf("Expected larger than 5, got %d", item[0].Id)
+	}
+}
+
+func TestRejectByItem(t *testing.T) {
+	data := initData()
+	item := RejectByItem(
+		data, func(e1 tempS) bool {
+			return e1.Id >= 5
+		},
+	)
+	if item[len(item)-1].Id >= 5 {
+		t.Errorf("Expected smaller than 5, got %d", item[len(item)-1].Id)
+	}
+}
+
+func initData() []*tempS {
+	array := []*tempS{}
+	for i := 0; i < 10; i++ {
+		array = append(array, &tempS{Id: i})
+	}
+	return array
 }
