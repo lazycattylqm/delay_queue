dfc82d9f003a14507ce0edfb8509ed9c54707787
LiQiming <leeqiming@163.com>
2022-06-07 16:35:29 +0800

    add test case for queue

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -30,3 +30,45 @@ func TestAddItem(t *testing.T) {
 	}
 
 }
+
+func TestDeleteCaseOne(t *testing.T) {
+	queue := New()
+	itemA := item.Item[any]{
+		Id:     "1",
+		Expire: 3000,
+		Data:   "test",
+	}
+	if err := queue.Add(itemA); err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	queue.DeleteItem(itemA)
+	if len(queue.Items) != 0 {
+		t.Errorf("Expected queue to be empty")
+	}
+}
+
+func TestFilter(t *testing.T) {
+	queue := New()
+	itemA := item.Item[any]{
+		Id:     "1",
+		Expire: 3000,
+		Data:   "test",
+	}
+	itemB := item.Item[any]{
+		Id:     "2",
+		Expire: 3000,
+		Data:   "test",
+	}
+	itemC := item.Item[any]{
+		Id:     "3",
+		Expire: 3000,
+		Data:   "test",
+	}
+	_ = queue.Add(itemA)
+	_ = queue.Add(itemB)
+	_ = queue.Add(itemC)
+	queue.FilterItems(itemA)
+	if len(queue.Items) != 1 {
+		t.Errorf("Expected queue to have 1 items")
+	}
+}
