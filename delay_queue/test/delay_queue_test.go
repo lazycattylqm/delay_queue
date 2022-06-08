0f203280f4310605798cf42a449cc5a24a033d1c
LiQiming <leeqiming@163.com>
2022-06-08 10:43:36 +0800

    delay queue 单元测试

--- delay_queue/test/delay_queue_test.go
+++ delay_queue/test/delay_queue_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"com.lqm.demo/delay_queue"
+	"com.lqm.go.demo/item"
 	"testing"
 )
 
@@ -17,5 +18,38 @@ func TestGetQueue(t *testing.T) {
 	q := dq.GetQueue()
 	if q == nil {
 		t.Error("GetQueue() should not be nil")
+	}
+}
+
+func TestOffer(t *testing.T) {
+	dq := delay_queue.New[string]()
+	i := item.New("id", 3000, "data")
+	dq.OfferTask(
+		*i, func(old, new item.Item[string]) item.Item[string] {
+			return new
+		},
+	)
+	if len(dq.GetQueue().Items) == 0 {
+		t.Error("OfferTask() should not be nil")
+	}
+
+}
+
+func TestOffer_Add_2_items(t *testing.T) {
+	dq := delay_queue.New[string]()
+	i := item.New("id", 3000, "data")
+	dq.OfferTask(
+		*i, func(old, new item.Item[string]) item.Item[string] {
+			return new
+		},
+	)
+	i2 := item.New("id2", 3000, "data")
+	dq.OfferTask(
+		*i2, func(old, new item.Item[string]) item.Item[string] {
+			return new
+		},
+	)
+	if len(dq.GetQueue().Items) != 2 {
+		t.Error("OfferTask() should not be nil")
 	}
 }
