280a80119798ab86105533e3029640aded4f40d1
LiQiming <leeqiming@163.com>
2022-06-14 19:28:30 +0800

    一个小型的防抖工具

---
+++ util_debounce/struct/debounce.go
@@ -0,0 +1,13 @@
+package _struct
+
+type DataNode[T any] struct {
+	Count int
+	Data  T
+}
+
+func Merge[T any](old, new DataNode[T], f func(e1, e2 T) T) DataNode[T] {
+	return DataNode[T]{
+		Count: old.Count + 1,
+		Data:  f(old.Data, new.Data),
+	}
+}
