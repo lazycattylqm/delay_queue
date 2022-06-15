4177da4b2bd1e0002ce25a7d6d1a39dcf80123c0
LiQiming <leeqiming@163.com>
2022-06-15 16:56:26 +0800

    struct test

--- util_debounce/test/debounce_test.go
+++ util_debounce/test/debounce_test.go
@@ -1,1 +1,24 @@
 package test
+
+import (
+	_struct "com.lqm.demo/util_debounce/struct"
+	"fmt"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	node := _struct.DataNode[string]{
+		Data: "this is node 1",
+	}
+
+	node2 := _struct.DataNode[string]{
+		Data: "this is node 2",
+	}
+
+	node3 := _struct.Merge(
+		node, node2, func(e1, e2 string) string {
+			return e1 + " " + e2
+		},
+	)
+	fmt.Println(node3)
+}
